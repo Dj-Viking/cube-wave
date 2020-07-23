eac41f7af8a2a24fd370a6018edad31c28140ba1
DJ Viking <anders.swedishviking@gmail.com>
2020-07-23 03:12:56 -0400

    close to what I am following so far

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,32 +1,44 @@
 let angle = 0;
 let w = 28.5;
+let ma
 
 function setup(){
-    createCanvas(400, 400, WEBGL);
+    createCanvas(500, 600, WEBGL);
+    ma = atan(1/sqrt(2))
 }
 
 function draw(){
-    background(0);
-    translate(0, 25, -50)
-    rotateX(PI / -8)
-    // translate(width / 2, height / 2);
-    rectMode(CENTER);
+    background(100);
+    ortho(-500, 500, 500, -500, 0, 1900);
+    //ambientLight(255, 0, 255, 1, -1, 10);
 
-    //rotateX(angle * 1.1);
+    // translate(0, 0, -100);
+    rotateX(QUARTER_PI);
+    rotateY(ma)
+    
+   
+
+    //rotateX(angle / 20);
 
     let offset = 0;
-    for (let x = 0; x < width; x += w){
-        push();
-        let a = angle + offset;
-        let h = map(sin(a), -1, 1, 0, 100);
-        fill(255);
-        translate(x - width / 2, 0, 0);
-        box(w, h, w);
-        // rect(x - width / 2 + w / 2, 0, w - 2, h)
-        offset += .4;
-        pop();
+    for (let z = 0; z < height; z += w){
+        for (let x = 0; x < width; x += w){
+            push();
+            let a = angle + offset;
+            let h = map(sin(a), -1, 1, 0, 100);
+            
+            translate(x - width / 2, 0, z - height / 2);
+            normalMaterial();
+            box(w - 2, h, w - 2);
+            //rect(x - width / 2 + w / 2, 0, w - 2, h)
+            
+            pop();
+            offset += .01;
+        }
+        // offset += .05;
     }
 
 
-    angle += .07;
+
+    angle += .05;
 }
