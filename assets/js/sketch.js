64f71734e8f56cd16bdf75b87990b7fdfb3e04d9
DJ Viking <anders.swedishviking@gmail.com>
2020-07-23 02:11:32 -0400

    cube wave starting point

---
+++ assets/js/sketch.js
@@ -0,0 +1,30 @@
+let angle = 0;
+let w = 28.5;
+
+function setup(){
+    createCanvas(400, 400, WEBGL);
+}
+
+function draw(){
+    background(0);
+    // translate(width / 2, height / 2);
+    rectMode(CENTER);
+
+    rotateX(angle * 1.1);
+
+    let offset = 0;
+    for (let x = 0; x < width; x += w){
+        push();
+        let a = angle + offset;
+        let h = map(sin(a), -1, 1, 0, 100);
+        fill(255);
+        translate(x - width / 2, 0, 0);
+        box(w, h, w);
+        // rect(x - width / 2 + w / 2, 0, w - 2, h)
+        offset += .03;
+        pop();
+    }
+
+
+    angle += .07;
+}
