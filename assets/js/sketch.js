adaf435250bd40196d19f327df4dc9054b8c189b
DJ Viking <anders.swedishviking@gmail.com>
2020-07-23 04:31:54 -0400

    looks pretty cool, this actually oworks in OBS later versions especially 25.0.8 on my mac which supports webGL

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,41 +1,52 @@
 let angle = 0;
-let w = 28.5;
-let ma
+let w = 30;
+let ma;
+let maxD;
 
 function setup(){
-    createCanvas(500, 600, WEBGL);
-    ma = atan(1/sqrt(2))
+    createCanvas(600, 600, WEBGL);
+    ma = atan(1/sqrt(2));
+    maxD = dist(0, 0, 250, 250);
 }
 
 function draw(){
-    background(100);
+    background('rgba(0, 0, 0, 0)');
     ortho(-500, 500, 500, -500, 0, 1900);
     //ambientLight(255, 0, 255, 1, -1, 10);
 
-    // translate(0, 0, -100);
-    rotateX(QUARTER_PI);
-    rotateY(ma)
+    //translate(0, 0, -100);
+    rotateX(QUARTER_PI + 6.3);
+    rotateY(ma + .15);
+    rotateZ(QUARTER_PI + 2.4);
     
    
 
-    //rotateX(angle / 20);
+    rotateX(angle / PI);
+    rotateY(angle / 2);
 
-    let offset = 0;
+    //let offset = 0;
     for (let z = 0; z < height; z += w){
         for (let x = 0; x < width; x += w){
             push();
+            let d = dist(x, z, width / 2, height / 2);
+            let offset = map(d, 0, maxD, PI, -PI);
             let a = angle + offset;
-            let h = map(sin(a), -1, 1, 0, 100);
+            let h = map(sin(a), -1, 1, 10, 500);
             
             translate(x - width / 2, 0, z - height / 2);
+            
             normalMaterial();
+            // torus(w, h, w - 2)
+            // ambientMaterial(0, 0, 255);
+            // ambientLight(255);
             box(w - 2, h, w - 2);
+            
             //rect(x - width / 2 + w / 2, 0, w - 2, h)
             
             pop();
-            offset += .01;
+            //offset += .2;
         }
-        // offset += .05;
+        //offset += .05;
     }
 
 
